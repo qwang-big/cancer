8bb4cddfb351f3d14b6e25d2485799e9af4b2979
qwang-big <qwang.big@gmail.com>
2021-03-25 16:14:33 +0800

    Update Genes_per_Spot.r

--- Genes_per_Spot.r
+++ Genes_per_Spot.r
@@ -13,5 +13,5 @@ fd=factor(seq_len(length(ff)))
 names(fd)=ff
 dd=do.call(rbind,lapply(ff[grep('gene$',ff)],function(f){d=read.table(f);d$S=substr(f,7,8);d$T=substr(f,10,10);d$R=fd[f];d}))
 
-p = ggplot(dd, aes(x=R, y=V1, color=T)) + geom_violin() + facet_wrap(~S,scales='free') + labs(title = 'Genes per Spot', x='Patient',y='')+theme(axis.text.x=element_blank())
+p = ggplot(dd, aes(x=R, y=V1, color=T)) + geom_violin() + facet_wrap(~S,scales='free_x') + labs(title = 'Genes per Bin50', x='Patient',y='')+theme(axis.text.x=element_blank())
 ggsave(p, filename='gene.pdf')

3fb096a4ed7227ece0af52c0e0bb53bde8edb688
qwang-big <qwang.big@gmail.com>
2021-06-29 19:55:01 +0800

    Update run_infercnvPlus.r

--- run_infercnvPlus.r
+++ run_infercnvPlus.r
@@ -18,11 +18,22 @@ print(dim(all))
 all$sample=substr(rownames(all@meta.data),start = 1,stop = 4)
 ref_obs=rownames(all@meta.data[all@meta.data$sample=='Norm',])
 print(str(ref_obs))
+rm(seurat_spatialObj)
+rm(Tumor_seurat)
+rm(Normal_seurat)
+gc()
 expr=as.matrix(all@assays$RNA@counts)
+nr <- nrow(expr)
+n <- ceiling(nr/20)
+i=rep(1:20, each=ceiling(nr/n), length.out=nr)
+rm(all)
+gc()
 counts_to_tpm <- function(x) t(t(x)*1e6/colSums(x))
-expr <- counts_to_tpm(expr)
+expr <- counts_to_tpm(expr[i==args[3],])
 expr_tr <- log2(expr + 1)
 expr_tr = round(expr_tr*10)
+rm(expr)
+gc()
 #load('FJ13_CNV.robj')
 # Data tranforming: genes(rows) X cells(columns)
 ## For 10X counts data 
@@ -35,10 +46,11 @@ cnv_obj <- inferCNV(data = expr_tr,
                     cutoff = 1, #0.1, # use 1 for smart-seq, 0.1 for 10x-genomics
                     reference_obs = ref_obs,
                     window_size = 101,
-                    out_path = "output_dir_new", # dir is auto-created for storing outputs
+                    out_path = "output_dir", # dir is auto-created for storing outputs
                     noise_filter = NULL,
                     vis_bounds = "-0.5,0.5")
-save(cnv_obj,file = paste0(args[1],".infered.cnv.rda"))
+rm(expr_tr)
+gc()
 #save(cnv_obj,file = 'FJ13_CNV_result_new_0.5.Robj')
 # Cluster cells and visualize
 cnv_obj <- visualCNV(data = cnv_obj,
@@ -50,15 +62,4 @@ cnv_obj <- extractCells(data = cnv_obj,
                         lab_to_rm = "ref")
 # Get cell barcode
 #cells <- cnv_obj$target
-save(cnv_obj,file = paste0(args[1],".res.cnv.rda"))
-load('LungCancer_T_FO14.robj')
-load('FO14_CNV_result_new_0.5_new.Robj')
-tree = as.data.frame(cutree(cnv_obj$cluster$hclust,k = 3))
-colnames(tree) = 'Cluster'
-tree$barcode = rownames(tree)
-tree = tree[cnv_obj$target,]
-Tumor = tree[tree$Cluster==3,]$barcode
-Tumor = Tumor[gsub('_.*','',Tumor) == 'Tumor']
-seurat_spatialObj@meta.data$cnv='low'
-seurat_spatialObj@meta.data[gsub('.*_','',Tumor),]$cnv='high'
-SpatialDimPlot(seurat_spatialObj,group.by = 'cnv',pt.size.factor = 2.5)
+saveRDS(cnv_obj,file = paste0(args[1],".",args[2],".",args[3],".rds"))

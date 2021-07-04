c7df52f6a78975f454fbf7180d5ed629a4e711cf
qwang-big <qwang.big@gmail.com>
2021-07-04 20:07:41 +0800

    Update plotCNV.r

--- CNV/plotCNV.r
+++ CNV/plotCNV.r
@@ -6,7 +6,6 @@ lw=function(d)length(which(d))
 df=data.frame(V1=ff[grep('rds$',ff)])
 df$f=unlist(lapply(df[,1],function(d) substr(d,1,13)))
 dd=split(df,df$f)
-dd=dd[1]
 lapply(dd, function(d){tryCatch({
 load(paste0(substr(d[1,1],1,4),'.rda'))
 Tumor=unlist(lapply(d[,1], function(s){
@@ -14,8 +13,10 @@ cnv_obj=readRDS(s)
 tree = as.data.frame(cutree(cnv_obj$cluster$hclust,k = n))
 colnames(tree) = 'Cluster'
 tree$barcode = rownames(tree)
-tree = tree[cnv_obj$target,]
-i=which.min(lapply(seq(1,max(tree$Cluster)),function(i)lw(tree$Cluster==i)))
+i=which.max(lapply(seq(1,max(tree$Cluster)),function(i) {
+tb=tree[tree$Cluster==i,'barcode']
+lw(cnv_obj$target %in% tb) / length(tb)
+}))
 Tumor = tree[tree$Cluster==i,]$barcode
 }))
 tt=table(Tumor)
@@ -25,9 +26,8 @@ Tumor = Tumor[gsub('_.*','',Tumor) == 'Tumor']
 print(length(Tumor))
 seurat_spatialObj@meta.data$cnv='low'
 seurat_spatialObj@meta.data[gsub('.*_','',Tumor),]$cnv='high'
-png(paste0(d[1,2],'.png'))
+png(paste0(gsub('rda','vs',d[1,2]),'.png'))
 p=SpatialDimPlot(seurat_spatialObj,group.by = 'cnv',pt.size.factor = 2.5)
 print(p)
 dev.off()}, error=function(e){})
 })
-

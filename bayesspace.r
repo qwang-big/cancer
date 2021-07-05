3097cf51eec02151cc267e3a1fb1c0da546b7f07
qwang-big <qwang.big@gmail.com>
2021-07-05 21:36:34 +0800

    Update bayesspace.r

--- bayesspace.r
+++ bayesspace.r
@@ -13,6 +13,18 @@ sce <- spatialPreprocess(sce, platform="ST",
 sce <- spatialCluster(sce, q=10, platform="ST", d=7,
                            init.method="mclust", model="t", gamma=2,
                            nrep=10000, burn.in=100, save.chain=TRUE)
-p3 <-SpatialDimPlot(seo, label = T, label.size = 3)+ labs(title="Seurat") +DarkTheme()
-p4 <- clusterPlot(sce,label = "spatial.cluster" )+ labs(title="BayesSpace")+DarkTheme()
-plot_grid(p3, p4)
+p3 <-SpatialDimPlot(seurat_spatialObj, label = T, label.size = 3) +DarkTheme()
+p4 <- clusterPlot(sce,label = "spatial.cluster",color=NA)+ DarkTheme()
+cowplot::plot_grid(p3, p4)
+
+c1=scran::findMarkers(sce, seurat_spatialObj@meta.data$seurat_cluster, pval.type="all")
+c2=scran::findMarkers(sce, colData(sce)$spatial.cluster, pval.type="all")
+n=1:10
+df=data.frame(lapply(n,function(i){
+x=rownames(c1[[i]][c1[[i]]$p.value < 0.05,])
+unlist(lapply(n,function(j){
+y=rownames(c2[[j]][c2[[j]]$p.value < 0.05,])
+length(intersect(x,y))/length(union(x,y))
+}))
+}))
+heatmap.2(as.matrix(df),Rowv=F,Colv=F,trace='none')

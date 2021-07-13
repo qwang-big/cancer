4546f442a0c49f38bd96233da2c62da7768d633d
qwang-big <qwang.big@gmail.com>
2021-07-13 11:06:01 +0800

    Update peak2gene.r

--- ATAC/peak2gene.r
+++ ATAC/peak2gene.r
@@ -1,6 +1,16 @@
+library(Seurat)
+library(ggplot2)
+library(patchwork)
+library(ArchR)
+library(cowplot)
+library(Signac)
+setwd("snATAC-seq/snATAC_cortex_TSS5_Frag1000/")
+atac <- readRDS("Save-ArchR-Project.rds")
+atac@sampleColData$ArrowFiles=dir("ArrowFiles")
+atac@sampleColData$ArrowFiles=paste0('ArrowFiles/',atac@sampleColData$ArrowFiles)
+names(atac@sampleColData$ArrowFiles)=substr(atac@sampleColData$ArrowFiles,1,nchar(atac@sampleColData$ArrowFiles)-6)
 x=read.csv('~/b/cortex_merge_meta.csv')
 ac=atac$cellNames
-dd=rownames(rna@meta.data)
 atac = subsetArchRProject(
   ArchRProj = atac,
   cells = ac[ac %in% x[,1]],
@@ -10,9 +20,12 @@ atac = subsetArchRProject(
   threads = getArchRThreads(),
   force = FALSE
 )
+saveRDS(atac, "Save-ArchR-Project.rds")
 rna <- readRDS("../../snRNA-seq/cortex_MTgene1.0%_UMI500_annotation.rds")
+dd = rownames(rna@meta.data)
 rna <- subset(rna, cells=dd[dd %in% x[,1]])
-ll=lapply(split(x,x$subtype_SCT),function(d){
+x=split(x,x$subtype_SCT)
+ll=lapply(x,function(d){
     SimpleList(
         ATAC = ac[ac %in% d[,1]],
         RNA = dd[dd %in% d[,1]]
@@ -28,5 +41,5 @@ atac <- addGeneIntegrationMatrix(
     seRNA = rna,
     addToArrow = FALSE, 
     groupList = groupList,
-    groupRNA = "BioClassification"
+    groupRNA = "celltype"
 )

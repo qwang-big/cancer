8f586ae83c1b04ef62c1eaa6296579aa0f5f493c
qwang-big <qwang.big@gmail.com>
2021-07-14 14:36:51 +0800

    Update subset.r

--- ATAC/subset.r
+++ ATAC/subset.r
@@ -1,6 +1,5 @@
 library(Seurat)
 library(ArchR)
-setwd("snATAC-seq/snATAC_cortex_TSS5_Frag1000/")
 atac <- readRDS("Save-ArchR-Project.rds")
 atac@sampleColData$ArrowFiles=dir("ArrowFiles")
 atac@sampleColData$ArrowFiles=paste0('ArrowFiles/',atac@sampleColData$ArrowFiles)
@@ -8,22 +7,23 @@ names(atac@sampleColData$ArrowFiles)=substr(atac@sampleColData$ArrowFiles,1,ncha
 cells <- unlist(lapply(atac@sampleColData$ArrowFiles,function(f) paste0(substr(f,1,nchar(f)-6),'#',h5read(f, "Metadata/CellNames"))))
 ac=getCellNames(atac)
 x=read.csv('~/b/cortex_merge_meta.csv')
-x=x[x[,1] %in% cells,]
+x=x[x[,1] %in% basename(cells),]
 ff=table(x$subtype_SCT)
 ff=names(ff)[ff>100]
-for(f in ff[2:28]){
+f="Astro_1"
+#for (f in ff){
 atac@projectMetadata$outputDirectory = paste0(f,'/tmp')
 dir.create(paste0(f,'/tmp'))
-atac1 = subsetArchRProject(
+atac = subsetArchRProject(
   ArchRProj = atac,
   cells = ac[ac %in% x[x$subtype_SCT==f,1]],
   outputDirectory = f,
   dropCells = TRUE,
   logFile = NULL,
-  threads = 6,
+  threads = 39,
   force = T
 )
-saveRDS(atac1, paste0(f,"/Save-ArchR-Project.rds"))
+saveRDS(atac, paste0(f,"/Save-ArchR-Project.rds"))
 }
 library(Seurat)
 rna <- readRDS("../../../snRNA-seq/cortex_MTgene1.0%_UMI500_annotation.rds")

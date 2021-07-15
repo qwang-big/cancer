097d65ed62e6fe6a4c6317aac0fdb8f7dc817590
qwang-big <qwang.big@gmail.com>
2021-07-15 09:59:57 +0800

    Update peak2gene.r

--- ATAC/peak2gene.r
+++ ATAC/peak2gene.r
@@ -6,9 +6,8 @@ library(cowplot)
 library(Signac)
 setwd("snATAC-seq/snATAC_cortex_TSS5_Frag1000/")
 atac <- readRDS("Save-ArchR-Project.rds")
-atac@sampleColData$ArrowFiles=dir("ArrowFiles")
-atac@sampleColData$ArrowFiles=paste0('ArrowFiles/',atac@sampleColData$ArrowFiles)
-names(atac@sampleColData$ArrowFiles)=substr(atac@sampleColData$ArrowFiles,1,nchar(atac@sampleColData$ArrowFiles)-6)
+ff=dir("ArrowFiles")
+atac@sampleColData <- DataFrame(row.names = substr(ff,1,nchar(ff)-6), ArrowFiles = paste0('ArrowFiles/',ff))
 x=read.csv('~/b/cortex_merge_meta.csv')
 ac=atac$cellNames
 atac = subsetArchRProject(

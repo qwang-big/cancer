3ec9392021e0ffcd9fa7dfd2e44428dd169d02e0
qwang-big <qwang.big@gmail.com>
2021-07-21 10:03:47 +0800

    Create tile.sh

---
+++ CNN/tile.sh
@@ -0,0 +1,11 @@
+python DeepPATH-master/DeepPATH_code/00_preprocessing/0b_tileLoop_deepzoom4.py -s 299 -e 0 -j 32 -B 25 -o sample10-T2-20x -N 89,9,-5,18,13,13 sample10-T2-20x.jpg 
+python DeepPATH-master/DeepPATH_code/00_preprocessing/0b_tileLoop_deepzoom4.py -s 299 -e 0 -j 32 -B 25 -o sample14-T2-20x -N 89,10,-6,16,12,13 sample14-T2-20x.jpg 
+python DeepPATH-master/DeepPATH_code/00_preprocessing/0b_tileLoop_deepzoom4.py -s 299 -e 0 -j 32 -B 25 -o sample15-T1-20x-无背景拼接 -N 90,7,-5,15,10,12 sample15-T1-20x-无背景拼接.jpg 
+python DeepPATH-master/DeepPATH_code/00_preprocessing/0b_tileLoop_deepzoom4.py -s 299 -e 0 -j 32 -B 25 -o sample16-T2-20x -N 90,7,-3,18,9,11 sample16-T2-20x.jpg 
+python DeepPATH-master/DeepPATH_code/00_preprocessing/0b_tileLoop_deepzoom4.py -s 299 -e 0 -j 32 -B 25 -o sample20-T2-20x -N 90,6,-5,17,9,11 sample20-T2-20x.jpg 
+python DeepPATH-master/DeepPATH_code/00_preprocessing/0b_tileLoop_deepzoom4.py -s 299 -e 0 -j 32 -B 25 -o sample22-T2-20x -N 90,9,-5,16,13,11 sample22-T2-20x.jpg 
+python DeepPATH-master/DeepPATH_code/00_preprocessing/0b_tileLoop_deepzoom4.py -s 299 -e 0 -j 32 -B 25 -o sample23-T-20x -N sample23-T-20x.jpg 
+python DeepPATH-master/DeepPATH_code/00_preprocessing/0b_tileLoop_deepzoom4.py -s 299 -e 0 -j 32 -B 25 -o sample24-T-20x -N 89,11,-4,19,15,12 sample24-T-20x.jpg 
+python DeepPATH-master/DeepPATH_code/00_preprocessing/0b_tileLoop_deepzoom4.py -s 299 -e 0 -j 32 -B 25 -o sample2-T-20x -N 91,9,-2,14,12,9 sample2-T-20x.jpg 
+python DeepPATH-master/DeepPATH_code/00_preprocessing/0b_tileLoop_deepzoom4.py -s 299 -e 0 -j 32 -B 25 -o sample8-T2-20x -N 91,9,-4,16,13,12 sample8-T2-20x.jpg 
+python DeepPATH-master/DeepPATH_code/00_preprocessing/0b_tileLoop_deepzoom4.py -s 299 -e 0 -j 32 -B 25 -o sample9-T2-20x -N 93,6,-2,13,10,10 sample9-T2-20x.jpg 

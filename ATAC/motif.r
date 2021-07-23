86ecbbf6ba9126a8705280cac7cdf456f2824eb7
qwang-big <qwang.big@gmail.com>
2021-07-23 10:19:12 +0800

    Update motif.r

--- ATAC/motif.r
+++ ATAC/motif.r
@@ -16,6 +16,13 @@ saveRDS(x, file="Annotations/Motif-Positions-In-Peaks.rds")
 x=readRDS("Annotations/Motif-Matches-In-Peaks.rds")
 seqlevels(x)<- sub('MFA','chr',seqlevels(x))
 saveRDS(x, file="Annotations/Motif-Matches-In-Peaks.rds")
-#write motif matrix to arrow files
+#compute 
 atac <- addBgdPeaks(atac)
 atac@cellColData$age_group=as.character(atac@cellColData$age_group)
+motifs <- c("SNAI1", "ZEB1", "TCF4", "ASCL1", "MYOD1", "Nr2f6")
+x=readRDS('Annotations/Motif-Positions-In-Peaks.rds')
+p=metadata(p2g)$peakSet[p2g$idxATAC]
+x=x[names(x)[unlist(lapply(motifs,function(d) grep(d,names(x))))]]
+p=metadata(p2g)$peakSet[p2g$idxATAC]
+g=metadata(p2g)$geneSet[p2g$idxRNA]
+df=do.call(rbind,lapply(seq_along(x),function(i) data.frame(names(x)[i],g[queryHits(findOverlaps(p,x[[i]]))]$name)))

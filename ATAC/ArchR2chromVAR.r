35018a9358e0d11569e34339a3432dc0719ede93
qwang-big <qwang.big@gmail.com>
2021-08-01 17:56:21 +0800

    Update ArchR2chromVAR.r

--- ATAC/ArchR2chromVAR.r
+++ ATAC/ArchR2chromVAR.r
@@ -1,13 +1,5 @@
-af.availableSeqnames = utils::getFromNamespace(".availableSeqnames", "ArchR")
-af.getRowSums = utils::getFromNamespace(".getRowSums", "ArchR")
-ArrowFiles <- getArrowFiles(atac)
-useMatrix <- "PeakMatrix"
-availableChr <- af.availableSeqnames(ArrowFiles, useMatrix)
-rS <- af.getRowSums(ArrowFiles = ArrowFiles, seqnames = availableChr, useMatrix = useMatrix, filter0 = FALSE)
-rS$start <- start(atac@peakSet)
-rS$end <- end(atac@peakSet)
-rS$GC <- atac@peakSet$GC
-se <- SummarizedExperiment::SummarizedExperiment(assays = SimpleList(counts = as.matrix(data.frame(rS$rowSums, 1))), rowData = DataFrame(bias = rS$GC, start = rS$start, end = rS$end))
-bgdPeaks <- chromVAR::getBackgroundPeaks(object = se, bias = rowData(se)$bias, iterations = 50, w = 0.1, bs = 50)
-motifs <- getJasparMotifs()
-motif_ix <- matchMotifs(motifs, counts_filtered, genome = BSgenome.Mfascicularis.NCBI.5.0)
+x=readRDS("Annotations/Motif-Matches-In-Peaks.rds")
+seqlevels(x)<- sub('chr','MFA',seqlevels(x))
+motifs <- TFBSTools::getMatrixSet(JASPAR2020::JASPAR2020, list(species=NULL, collection="CORE"))
+genome = BSgenome.Mfascicularis.NCBI.5.0::BSgenome.Mfascicularis.NCBI.5.0
+motif_ix <- matchMotifs(motifs, x, genome)

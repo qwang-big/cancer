d740fc2d8f67cd4a8a993265eb68d7718e59c643
qwang-big <qwang.big@gmail.com>
2021-08-04 14:33:38 +0800

    Update ArchR2chromVAR.r

--- ATAC/ArchR2chromVAR.r
+++ ATAC/ArchR2chromVAR.r
@@ -1,5 +1,26 @@
+markerTest <- getMarkerFeatures(
+  ArchRProj = atac, 
+  useMatrix = "PeakMatrix",
+  groupBy = "age_group",
+  testMethod = "wilcoxon",
+  bias = c("TSSEnrichment", "log10(nFrags)"),
+  useGroups = "4_Geriatric",
+  bgdGroups = "3_Old"
+)
+pma <- markerPlot(seMarker = markerTest, name = "4_Geriatric", cutOff = "FDR <= 0.1 & abs(Log2FC) >= 1", plotAs = "MA")
+pma
+atac@peakAnnotation$Motif$Positions="Annotations/Motif-Positions-In-Peaks.rds"
+atac@peakAnnotation$Motif$Matches="Annotations/Motif-Matches-In-Peaks.rds"
+seqlevels(projHeme5@peakSet)<- sub('MFA','chr',seqlevels(projHeme5@peakSet))
+motifsUp <- peakAnnoEnrichment(
+    seMarker = markerTest,
+    ArchRProj = atac,
+    peakAnnotation = "Motif",
+    cutOff = "FDR <= 0.1 & Log2FC >= 0.5"
+  )
 x=readRDS("Annotations/Motif-Matches-In-Peaks.rds")
 seqlevels(x)<- sub('chr','MFA',seqlevels(x))
 motifs <- TFBSTools::getMatrixSet(JASPAR2020::JASPAR2020, list(species=NULL, collection="CORE"))
 genome = BSgenome.Mfascicularis.NCBI.5.0::BSgenome.Mfascicularis.NCBI.5.0
+dd=unlist(lapply(motifs,TFBSTools::name))
 motif_ix <- matchMotifs(motifs, x, genome)

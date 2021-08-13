dfd4df69e228050124c0dee2f3572d7d37aeee29
qwang-big <qwang.big@gmail.com>
2021-08-13 08:57:48 +0800

    Create DApeak.r

---
+++ ATAC/DApeak.r
@@ -0,0 +1,36 @@
+x=read.csv('~/b/hippo_ATAC_cellColdata.csv')
+rownames(x)=x[,1]
+atac@cellColData$celltype=x[rownames(atac@cellColData),'merge_subtype_SCT']
+ff=dir("ArrowFiles")
+atac@sampleColData <- DataFrame(row.names = substr(ff,1,nchar(ff)-6), ArrowFiles = paste0('ArrowFiles/',ff))
+dd=unique(atac@cellColData$celltype)
+
+for (d in dd[-1]) {
+atac <- readRDS("Save-ArchR-Project.rds")
+atac@cellColData$celltype=x[rownames(atac@cellColData),'merge_subtype_SCT']
+ff=dir("ArrowFiles")
+atac@sampleColData <- DataFrame(row.names = substr(ff,1,nchar(ff)-6), ArrowFiles = paste0('ArrowFiles/',ff))
+dd=unique(atac@cellColData$celltype)
+ac=getCellNames(atac)
+a = subsetArchRProject(
+  ArchRProj = atac,
+  cells = ac[ac %in% x[x$merge_subtype_SCT==d,1]],
+  outputDirectory = d,
+  dropCells = TRUE,
+  logFile = NULL,
+  threads = getArchRThreads(),
+  force = T
+)
+markersPeaks <- getMarkerFeatures(
+    ArchRProj = a, 
+    useMatrix = "PeakMatrix", 
+    groupBy = "age_group",
+  bias = c("TSSEnrichment", "log10(nFrags)"),
+  testMethod = "wilcoxon"
+)
+
+markerList <- getMarkers(markersPeaks, cutOff = "FDR <= 0.05 & Log2FC >= 1")
+for(i in 1:4)
+write.table(markerList[[i]],file=paste0(d,'/',i,'.bed'),sep='\t',quote=F,col.names=F)
+
+}

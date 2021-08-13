15bba53001f472944d763c9abb27dc1773122309
qwang-big <qwang.big@gmail.com>
2021-08-13 17:03:27 +0800

    Update DApeak.r

--- ATAC/DApeak.r
+++ ATAC/DApeak.r
@@ -5,8 +5,12 @@ ff=dir("ArrowFiles")
 atac@sampleColData <- DataFrame(row.names = substr(ff,1,nchar(ff)-6), ArrowFiles = paste0('ArrowFiles/',ff))
 dd=unique(atac@cellColData$celltype)
 rownames(atac@cellColData)=substr(rownames(atac@cellColData),23,nchar(rownames(atac@cellColData)))
-atac <- addGroupCoverages(ArchRProj = atac, groupBy = "age_group")
 atac@projectMetadata$outputDirectory='./tmp'
+atac <- addGroupCoverages(ArchRProj = atac, groupBy = "age_group")
+atac <- addReproduciblePeakSet(
+  ArchRProj = atac, groupBy = 'age_group', peakMethod = 'Macs2',
+  pathToMacs2 = '/hwfssz5/ST_PRECISION/OCG/wangqi/miniconda3/envs/py36/bin/macs2'
+)
 for (d in dd[-1]) {
 atac <- readRDS("Save-ArchR-Project.rds")
 atac@cellColData$celltype=x[rownames(atac@cellColData),'merge_subtype_SCT']
